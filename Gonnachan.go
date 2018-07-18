16fcba515cbf322b22d5b9729f2ffda6d5b9d042
insomnyawolf <ruben.tokyo1999@gmail.com>
2018-07-19 00:36:26 +0200

    Code Improovement \:D/

--- Gonnachan.go
+++ Gonnachan.go
@@ -1,4 +1,4 @@
-package main
+package gonnachan
 
 import (
 	"bytes"
@@ -12,55 +12,62 @@ import (
 )
 
 const (
+	//APIurl Base address for api requests
 	APIurl = "http://konachan.com/post.json?"
+
+	//Rating
+
+	//RatingSafe PG
+	RatingSafe = "s"
+	//RatingQuestionable +16
+	RatingQuestionable = "q"
+	//RatingExplicit +18
+	RatingExplicit = "e"
 )
 
+//KonachanPostRequest store data prepare the api querry
 type KonachanPostRequest struct {
-	tags       []string
-	randOrder  bool
-	rating     string
-	height     int
-	width      int
-	maxResults int
+	Tags       []string
+	RandOrder  bool
+	Rating     string
+	Height     int
+	Width      int
+	MaxResults int
 }
 
+//APIrequest parse KonachanPostRequest to get the equivalent api query url
 func (c *KonachanPostRequest) APIrequest() string {
 
 	tags := ""
 
-	if c.randOrder {
+	if c.RandOrder {
 		tags += "order%3Arandom+"
 	}
 
-	for _, t := range c.tags {
+	for _, t := range c.Tags {
 		tags += t + "+"
 	}
 
-	if c.rating != "" {
-		tags += fmt.Sprintf("rating%%3A%v", c.rating)
+	if c.Rating != "" {
+		tags += fmt.Sprintf("rating%%3A%v", c.Rating)
 	}
 
-	if c.height != 0 {
-		tags += fmt.Sprintf("height%%3A%v+", c.height)
+	if c.Height != 0 {
+		tags += fmt.Sprintf("height%%3A%v+", c.Height)
 	}
 
-	if c.width != 0 {
-		tags += fmt.Sprintf("width%%3A%v", c.width)
+	if c.Width != 0 {
+		tags += fmt.Sprintf("width%%3A%v", c.Width)
 	}
 
-	if c.maxResults == 0 {
-		c.maxResults = 1
+	if c.MaxResults == 0 {
+		c.MaxResults = 1
 	}
 
-	return fmt.Sprintf("%vlimit=%v&tags=%v", APIurl, strconv.Itoa(c.maxResults), tags)
+	return fmt.Sprintf("%vlimit=%v&tags=%v", APIurl, strconv.Itoa(c.MaxResults), tags)
 }
 
-type KonachanPostResult struct {
-	id  string
-	url string
-}
-
-//Search a pic in Image Booru sites api
+//GetResults runs the query obtained at APIrequest and returns KonachanPostResult
 func (c *KonachanPostRequest) GetResults() []KonachanPostResult {
 	URL := c.APIrequest()
 	res, err := http.Get(URL)
@@ -78,21 +85,42 @@ func (c *KonachanPostRequest) GetResults() []KonachanPostResult {
 
 	var results []KonachanPostResult
 
-	for x := 0; x < c.maxResults; x++ {
-		id := gjson.GetBytes(thing, fmt.Sprintf("%v.id", x)).String()
-		url := gjson.GetBytes(thing, fmt.Sprintf("%v.file_url", x)).String()
-		//Fix url for konachan sites wich doesn't start with http: on the file url
-		if !strings.HasPrefix(url, "http") && url != "" {
-			url = "http:" + url
+	for x := 0; x < c.MaxResults; x++ {
+
+		r := KonachanPostResult{
+			ID:       gjson.GetBytes(thing, fmt.Sprintf("%v.id", x)).Int(),
+			Tags:     gjson.GetBytes(thing, fmt.Sprintf("%v.tags", x)).String(),
+			Author:   gjson.GetBytes(thing, fmt.Sprintf("%v.author", x)).String(),
+			Source:   gjson.GetBytes(thing, fmt.Sprintf("%v.source", x)).String(),
+			Score:    gjson.GetBytes(thing, fmt.Sprintf("%v.score", x)).Int(),
+			Md5:      gjson.GetBytes(thing, fmt.Sprintf("%v.md5", x)).String(),
+			FileSize: gjson.GetBytes(thing, fmt.Sprintf("%v.file_size", x)).Int(),
+			FileURL:  gjson.GetBytes(thing, fmt.Sprintf("%v.file_url", x)).String(),
+			Rating:   gjson.GetBytes(thing, fmt.Sprintf("%v.rating", x)).String(),
+			Width:    gjson.GetBytes(thing, fmt.Sprintf("%v.width", x)).Int(),
+			Height:   gjson.GetBytes(thing, fmt.Sprintf("%v.height", x)).Int(),
 		}
-		results = append(results, KonachanPostResult{id: id, url: url})
+		//Fix url for konachan sites which doesn't start with http: on the file url
+		if !strings.HasPrefix(r.FileURL, "http") && r.FileURL != "" {
+			r.FileURL = "http:" + r.FileURL
+		}
+		results = append(results, r)
 	}
 
 	return results
 }
 
-func (c *KonachanPostResult) SearchID(id int) []KonachanPostResult {
-	tag := []string{fmt.Sprintf("id:%v", id)}
-	req := KonachanPostRequest{tags: tag}
-	return req.GetResults()
+//KonachanPostResult has usefull data obtained from the API
+type KonachanPostResult struct {
+	ID       int64  `json:"id"`
+	Tags     string `json:"tags"`
+	Author   string `json:"author"`
+	Source   string `json:"source"`
+	Score    int64  `json:"score"`
+	Md5      string `json:"md5"`
+	FileSize int64  `json:"file_size"`
+	FileURL  string `json:"file_url"`
+	Rating   string `json:"rating"`
+	Width    int64  `json:"width"`
+	Height   int64  `json:"height"`
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Gonnachan
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/insomnyawolf/Gonnachan)](https://goreportcard.com/report/github.com/insomnyawolf/Gonnachan)
+
 Konachan api written in go WORK IN PROGRESS
+
+Working:
+    PostSearch
