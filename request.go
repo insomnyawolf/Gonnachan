c019199bacf0b13a2cd3d767570d4b02bdd6a98a
Unknown <ruben.tokyo1999@gmail.com>
2019-01-07 17:54:21 +0100

    Huge rework

---
+++ request.go
@@ -0,0 +1,176 @@
+package gonnachan
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var (
+	serversGelboru  = []string{ServerGelbooru, ServerSafebooru, ServerRule34}
+	serversKonachan = []string{ServerKonachan, ServerYandere}
+)
+
+const (
+	//ServerKonachan APIendPoint
+	ServerKonachan = "http://konachan.com/"
+	//ServerYandere APIendPoint
+	ServerYandere = "https://yande.re/"
+	//ServerGelbooru APIendPoint
+	ServerGelbooru = "https://gelbooru.com/"
+	//ServerSafebooru APIendPoint
+	ServerSafebooru = "https://safebooru.org/"
+	//ServerRule34 APIendPoint
+	ServerRule34 = "https://rule34.xxx/"
+
+	//Common query to get post result in similar pages
+	endpointGelboru  = "index.php?page=dapi&s=post&q=index&json=1&"
+	endpointKonachan = "post.json?"
+	//Enum Alternative
+	typeUnsupported = -1
+	typeKonachan    = 0
+	typeGelboru     = 1
+
+	//Danbooru APIendPoint
+	//Danbooru = "https://danbooru.donmai.us/posts/1.json"
+	//Im too lazy to fix this at the moment.
+
+	//ToDo
+	//chan.sankakucomplex.com
+	//idol.sankakucomplex.com
+
+	//e-shuushuu.net
+
+	//anime-pictures.net
+
+	//danbooru.donmai.us
+
+	//www.zerochan.net
+)
+
+const (
+	//Rating
+
+	//RatingSafe PG
+	RatingSafe = "s"
+	//RatingQuestionable +16
+	RatingQuestionable = "q"
+	//RatingExplicit +18
+	RatingExplicit = "e"
+)
+
+const (
+	//ErrNoResults No results found
+	ErrNoResults = 1
+	//ErrEmpty No answer from server
+	ErrEmpty = 2
+)
+
+var (
+	//URL Base address for the requests
+	URL string
+)
+
+//KonachanPostRequest store data prepare the api querry
+type KonachanPostRequest struct {
+	Tags       []string
+	BeforeID   int64
+	AfterID    int64
+	RandOrder  bool
+	Rating     string
+	Height     int
+	Width      int
+	MaxResults int
+	TargetAPI  string
+	serverType int
+}
+
+//Close EOL
+func (c *KonachanPostRequest) Close() {
+	c = nil
+}
+
+//APIrequest parse KonachanPostRequest to get the equivalent api query url
+func (c *KonachanPostRequest) APIrequest() string {
+	c.serverType = c.getServerKind()
+	tags := strings.Join(c.Tags, "+")
+
+	if tags != "" {
+		tags += "+"
+	}
+
+	if c.BeforeID != 0 {
+		tags += fmt.Sprintf("id:<%v+", c.BeforeID)
+	}
+
+	if c.AfterID != 0 {
+		tags += fmt.Sprintf("id:>%v+", c.AfterID)
+	}
+
+	if c.RandOrder {
+		tags += "order%3Arandom+"
+	}
+
+	if c.Rating != "" {
+		tags += fmt.Sprintf("rating%%3A%v+", c.Rating)
+	}
+
+	if c.Height != 0 {
+		tags += fmt.Sprintf("height%%3A%v+", c.Height)
+	}
+
+	if c.Width != 0 {
+		tags += fmt.Sprintf("width%%3A%v+", c.Width)
+	}
+
+	if c.MaxResults == 0 {
+		c.MaxResults = 1
+	}
+
+	query := fmt.Sprintf("limit=%v&tags=%v", strconv.Itoa(c.MaxResults), tags)
+	if c.TargetAPI == "" {
+		URL = ServerGelbooru
+	} else {
+		URL = c.TargetAPI
+	}
+	var endpoint string
+	if c.serverType == typeKonachan {
+		endpoint = endpointKonachan
+	} else if c.serverType == typeGelboru {
+		endpoint = endpointGelboru
+	}
+	uri := URL + endpoint + query
+	return uri
+}
+
+//RatingString Returns the human-readable sting for rating values
+func (c *KonachanPostResult) RatingString() string {
+	switch c.Rating {
+	case RatingSafe:
+		return "Safe"
+	case RatingQuestionable:
+		return "Questionable"
+	case RatingExplicit:
+		return "Explicit"
+	default:
+		return ""
+	}
+}
+
+func (c *KonachanPostRequest) getServerKind() int {
+	if contains(serversKonachan, c.TargetAPI) {
+		return typeKonachan
+	} else if contains(serversGelboru, c.TargetAPI) {
+		return typeGelboru
+	}
+	return typeUnsupported
+}
+
+func contains(strList []string, str string) bool {
+	for _, a := range strList {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
---
+++ results.go
@@ -0,0 +1,99 @@
+package gonnachan
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/tidwall/gjson"
+)
+
+//GetResults runs the query obtained at APIrequest and returns KonachanPostResult
+func (c *KonachanPostRequest) GetResults() ([]KonachanPostResult, error) {
+	URL := c.APIrequest()
+	res, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	thing, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if string(thing) == "[]" {
+		return nil, errors.New("No results")
+	}
+	if string(thing) == "" {
+		return nil, errors.New("Empty Response")
+	}
+
+	//Single alocation
+	var results []KonachanPostResult
+	for x := 0; x < c.MaxResults; x++ {
+		id := gjson.GetBytes(thing, fmt.Sprintf("%v.id", x))
+		if !id.Exists() {
+			break
+		}
+		r := KonachanPostResult{
+			ID:       id.Int(),
+			Tags:     gjson.GetBytes(thing, fmt.Sprintf("%v.tags", x)).String(),
+			Author:   gjson.GetBytes(thing, fmt.Sprintf("%v.author", x)).String(),
+			Source:   gjson.GetBytes(thing, fmt.Sprintf("%v.source", x)).String(),
+			Score:    gjson.GetBytes(thing, fmt.Sprintf("%v.score", x)).Int(),
+			FileSize: gjson.GetBytes(thing, fmt.Sprintf("%v.file_size", x)).Int(),
+			Rating:   gjson.GetBytes(thing, fmt.Sprintf("%v.rating", x)).String(),
+			Width:    gjson.GetBytes(thing, fmt.Sprintf("%v.width", x)).Int(),
+			Height:   gjson.GetBytes(thing, fmt.Sprintf("%v.height", x)).Int(),
+		}
+
+		//Server Specific Code
+		if c.serverType == typeKonachan {
+			//Md5
+			r.Md5 = gjson.GetBytes(thing, fmt.Sprintf("%v.md5", x)).String()
+			//ImageUrl
+			r.FileURL = gjson.GetBytes(thing, fmt.Sprintf("%v.file_url", x)).String()
+		} else if c.serverType == typeGelboru {
+			//ImageUrl
+			image := gjson.GetBytes(thing, fmt.Sprintf("%v.image", x)).String()
+			directory := gjson.GetBytes(thing, fmt.Sprintf("%v.directory", x)).String()
+			r.FileURL = fmt.Sprintf("%s/images/%s/%s", c.TargetAPI, directory, image)
+			//Md5
+			r.Md5 = gjson.GetBytes(thing, fmt.Sprintf("%v.hash", x)).String()
+		}
+
+		//Fix url for konachan sites which doesn't start with http: on the file url
+		if !strings.HasPrefix(r.FileURL, "http") && r.FileURL != "" {
+			r.FileURL = "http:" + r.FileURL
+		}
+		results = append(results, r)
+	}
+	return results, nil
+}
+
+//KonachanPostResult has useful data obtained from the API
+type KonachanPostResult struct {
+	ID       int64  `json:"id"`
+	Tags     string `json:"tags"`
+	Author   string `json:"author"`
+	Source   string `json:"source"`
+	Score    int64  `json:"score"`
+	Md5      string `json:"md5"`
+	FileSize int64  `json:"file_size"`
+	FileURL  string `json:"file_url"`
+	Rating   string `json:"rating"`
+	Width    int64  `json:"width"`
+	Height   int64  `json:"height"`
+}
+
+//Close EOL
+func (c *KonachanPostResult) Close() {
+	c = nil
+}
+
+//GetPostURL returns Konachan post url
+func (c *KonachanPostResult) GetPostURL() string {
+	return fmt.Sprintf("%v/post/show/%v", URL, c.ID)
+}
